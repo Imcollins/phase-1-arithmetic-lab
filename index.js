3c9baa2d522646e0024af8d7b6cd6a921fd68ddf
collins kipngeno <kipngenolins01@gmail.com>
2024-04-02 18:34:03 +0300

    Arithmetic.js

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply =(num1 * num2);
+console.log(multiply);
+
+//=>:62
+
+
+
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+//=>0
+
+
+
+let num3 = 14;
+let num4 = 5;
+let mod = (num3 %num4);
+
+console.log(mod);
+//=>: 4
+
+
+
+let numbers = (10, 15, 20, 5);
+let max = Math.max (10, 15, 20, 5);
+console.log (max);
+//=> 20
